83003ebfd20ea127756f474c3e82a1324a390ab8
BenC0 <suddensquirrel94@gmail.com>
2022-05-09 11:38:05 +0100

    Built v1 and added comments

--- src/variation_1/index.js
+++ src/variation_1/index.js
@@ -1,24 +1,45 @@
 import variationCSS from "./index.css";
+// Importing the norman library
 import norman from "../norman/index.js"
 
+// Declare my init function
 function init() {
+    // Log "Variation 1" to the window norman object
     norman.core.log("Variation 1")
+    // Track the Variant impression event
     norman.core.track(Variant.name, "Loaded", true)
+    // Get all titles
+    let title = norman.core.elementManagement.get('.best-sellers__title')
+    // Change all titles
+    title.forEach(t => t.textContent = "Shop by pet type")
 }
 
+// Declare the variant with all the details.
 const Variant = {
     name: "Variation 1",
+    // Set the Varaint CSS to an empty string, this prevents CSS from being added to the page.
     css: variationCSS,
+    // Declaring our conditions function for polling.
     conditions: () => {
         let conditions = []
-        conditions.push(true)
+        // Return true/false if element exists
+        conditions.push(!!document.querySelector('.best-sellers__title'))
+        // Return true/false if element exists
+        conditions.push(norman.core.elementManagement.exists('.best-sellers__title'))
+        // logging the conditions
         norman.core.log({message: `Polling: Conditions`, conditions})
+        // check every element in array is true
         let result = conditions.every(a => a)
+        // logging the result
         norman.core.log({message: `Polling: Result`, result})
+        // return the result
         return result
     },
+    // Map init function to actions property
     actions: init,
 }
 
+// Register variant in norman framework
 let nVariant = norman.init(Variant)
+// Run variant
 nVariant.run()
